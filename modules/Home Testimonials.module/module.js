39ff1f7fdc176df43a792ee37bff643fc885ab90
SolarteDev602 <solarteacostaw@gmail.com>
2023-04-20 10:32:50 -0500

    added arrows to slider on home page

--- modules/Home Testimonials.module/module.js
+++ modules/Home Testimonials.module/module.js
@@ -1,8 +1,12 @@
-var glide = new Glide('.testimonial-glide', {
+var glide = new Glide('.glide', {
   type: 'carousel',
   startAt: 0,
   perView: 1,
   // autoplay: 6000,
+  arrows: {
+    prev: ".glide__arrow--left",
+    next: ".glide__arrow--right",
+  },
 })
 
 glide.mount();
